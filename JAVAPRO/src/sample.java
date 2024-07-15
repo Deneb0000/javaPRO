999285a09f6d3351deb779de6ab7168a39b4b0f3
Deneb0000 <119793723+Deneb0000@users.noreply.github.com>
2024-07-15 15:32:05 +0900

    sample코딩추가
    sample코딩추가

--- JAVAPRO/src/sample.java
+++ JAVAPRO/src/sample.java
@@ -4,6 +4,7 @@ public class sample {
 	public static void main(String[] args) {
 		System.out.println("김인경");
 		System.out.println("KLK"); //ㅇ
+		System.out.println("누군가에 의해서 코딩을 추가 되었다"); //ㅇ
 
 	}
 
